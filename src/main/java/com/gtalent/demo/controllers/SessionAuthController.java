03edecf9f2fb1caba240db90383d94306d0e5b78
RunRunDog-Simon <g4ru4j32l4@gmail.com>
2025-08-16 11:04:26 +0800

    session.setAttribute

--- src/main/java/com/gtalent/demo/controllers/SessionAuthController.java
+++ src/main/java/com/gtalent/demo/controllers/SessionAuthController.java
@@ -65,6 +65,7 @@ public class SessionAuthController {
         user.setPwd(request.getPassword().trim());
         User savedUser = userRepository.save(user);
         session.setAttribute("userId" , user.getId());
+        //此處代表登入後，直接保留登入狀態
         return ResponseEntity.ok(user);
     }
 
